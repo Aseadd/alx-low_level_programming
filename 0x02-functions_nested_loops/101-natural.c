2eca21323eec1636f04d0565d39ecf9e0e8cd11f
Aseadd <ada@Ada.localdomain>
2022-03-29 22:50:56 +0300

    This is a pointer

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+	int sum3=0,sum7=0;
+	int 1;
+	for(i=0;i<1024;i++)
+{
+		if(i%3==0){
+			sum+=i;
+}		
+		if(i%5==0){
+	}
+}
+	printf("%d\n",sum3+sum7);
+	return 0;
+}
